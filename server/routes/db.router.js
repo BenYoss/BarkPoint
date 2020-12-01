b639321d50ccc33995758d0445634ebe32a6992b
benyoss <benyoss4@gmail.com>
2020-12-01 14:30:05 -0600

    (fix) merge conflicts

--- server/routes/db.router.js
+++ server/routes/db.router.js
@@ -32,7 +32,7 @@ dbRouter.get('/data/dog', (req, res) => {
 // add a new dog to database
 dbRouter.post('/data/dog', (req, res) => {
   const {
-    size, breed, number, dogname, emailUser, personalitytypes,
+    size, breed, number, dogname, personalitytypes, emailUser,
   } = req.body;
   return Dog.addDog(dogname, breed, size, number, emailUser, personalitytypes)
     .then(() => {
